554c02de7e86aea218828deb7d88a185aaa81ae8
GoBeyondTo <m18271739454@163.com>
2019-03-25 22:11:29 +0800

    test

---
+++ src/com/Test.java
@@ -0,0 +1,12 @@
+package com;
+
+/**
+ * @Author: E-mail:
+ * @Version: 创建时间：2019年03月25日22:10
+ * @Description: <描述>
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
